3bb32638635beec2eb1f8475fb3394222d490e74
noDynoGollum <muellersarahlouise64@gmail.com>
2025-09-15 21:59:50 +0200

    panda

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "/image.png";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
